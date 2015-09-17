3a3c3bd8f588ae316ef660b313d81ca773b509a5
bstriker <strikeforcezero@gmail.com>
2015-09-17 13:36:35 -0400

    Make sure www exists after a clean task

--- gulp/tasks/clean.js
+++ gulp/tasks/clean.js
@@ -3,9 +3,14 @@
 var config = require('../config');
 var gulp   = require('gulp');
 var del    = require('del');
+var file   = require('gulp-file');
 
 gulp.task('clean', function(cb) {
 
-  del([config.dist.root], cb);
+  del([config.dist.root], function(){
+	//Cordova requires www, so lets keep the folder using .gitkeep even if its empty after a clean.
+	file('.gitkeep', '', { src: true }).pipe(gulp.dest(config.dist.root));
+	cb();
+  });
 
 });

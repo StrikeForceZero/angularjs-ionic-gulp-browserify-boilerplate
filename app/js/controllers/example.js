d5e75cd6c5f859ed71af5db9bdf9d361c6992bfe
bstriker <strikeforcezero@gmail.com>
2015-11-02 21:02:16 -0500

    Cleaned up example controller and home view for cordova examples

--- app/js/controllers/example.js
+++ app/js/controllers/example.js
@@ -1,44 +1,34 @@
 'use strict';
 
-function ExampleCtrl($cordovaDevice) {
+import angular from 'angular';
+
+function ExampleCtrl($scope, $cordovaDevice) {
 
   // ViewModel
   const vm = this;
 
   vm.title = 'AngularJS, Cordova, Gulp, and Browserify! Written with keyboards and love!';
   vm.number = 1234;
-  vm.deviceReady = 'Cordova not loaded';
+  vm.deviceReady = false;
+  vm.deviceReadyStatus  ='Cordova not loaded';
+  vm.deviceInfo = {};
 
-  document.addEventListener('deviceready', function () {
+  vm.loadDeviceInfo = function(){
+    $scope.$apply(function () { //lets make sure angular hears about our update
+      vm.deviceReady = true;
+      vm.deviceReadyStatus = 'Device Ready';
 
-    vm.deviceReady = 'Device Ready - ';
-
-    try {
-      angular.isDefined($cordovaDevice.getDevice()); //unfortunately if the plugin is not installed calling this will cause fatal error
-      vm.deviceReady += 'Check console for device information';
-    }
-    catch (e) {
-      vm.deviceReady += 'Plugin not installed, please run "cordova plugin add cordova-plugin-device"';
-      return;
-    }
-
-    var device = $cordovaDevice.getDevice(),
-        cordova = $cordovaDevice.getCordova(),
-        model = $cordovaDevice.getModel(),
-        platform = $cordovaDevice.getPlatform(),
-        uuid = $cordovaDevice.getUUID(),
-        version = $cordovaDevice.getVersion();
-
-    console.log({
-      "device": device,
-      "cordova": cordova,
-      "model": model,
-      "platform": platform,
-      "uuid": uuid,
-      "version": version
+      try {
+        angular.isDefined($cordovaDevice.getDevice()); //unfortunately if the plugin is not installed calling this will cause fatal error
+        vm.deviceInfo = $cordovaDevice.getDevice();
+      }
+      catch (e) {
+        vm.deviceReadyStatus += ' - Plugin not installed, please run "cordova plugin add cordova-plugin-device"';
+      }
     });
+  };
 
-  }, false);
+  document.addEventListener('deviceready', vm.loadDeviceInfo, false);
 }
 
 export default {

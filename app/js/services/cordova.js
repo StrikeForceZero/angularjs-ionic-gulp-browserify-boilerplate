9099abdbb9a5fcc76513326ae8a824086646b4b0
bstriker <strikeforcezero@gmail.com>
2015-11-10 16:30:36 -0500

    Add angular service that contains a promise for the cordova deviceready event

---
+++ app/js/services/cordova.js
@@ -0,0 +1,38 @@
+'use strict';
+
+// Based off:
+// http://stackoverflow.com/a/21906525/1443717
+// https://github.com/aderowbotham/angular-cordova-bootstrap/blob/master/www/js/services/fs-cordova.js
+function CordovaService($document, $timeout, $window, $q) {
+
+  const defer = $q.defer();
+
+  let self = this;
+
+  self.ready = defer.promise;
+
+  // Backup in the case that we did not received the event
+  // This seemed to be necessary with some versions of Cordova
+  // when testing via 'cordova serve' in a web browser
+  // but when on-device the event is received correctly
+  let timeoutPromise = $timeout(() => {
+    if ($window.cordova){
+      defer.resolve($window.cordova);
+    } else {
+      defer.reject("Cordova failed to load");
+    }
+  }, 1200);
+
+  angular.element($document)[0].addEventListener('deviceready', () => {
+    $timeout.cancel(timeoutPromise);
+    defer.resolve($window.cordova);
+  });
+
+  return self;
+
+}
+
+export default {
+  name: 'CordovaService',
+  fn: CordovaService
+};
